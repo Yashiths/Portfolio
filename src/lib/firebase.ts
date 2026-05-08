73b3172b62e9598bba091a2a992c7abf3c860898
Yashiths <108421984+Yashiths@users.noreply.github.com>
2026-05-08 17:27:17 +0530

    update firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,23 @@
-import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
-import { getAuth, Auth } from "firebase/auth";
-import { getFirestore, Firestore } from "firebase/firestore";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
+  apiKey: "AIzaSyBt1eWkTFeU3BSXVyQLjSRCdkUPbEbrNJQ",
+  authDomain: "yashith-portfolio.firebaseapp.com",
+  projectId: "yashith-portfolio",
+  storageBucket: "yashith-portfolio.firebasestorage.app",
+  messagingSenderId: "125827066205",
+  appId: "1:125827066205:web:8812376ca9274e01bce0b4",
+  measurementId: "G-C700FDK87B"
 };
 
 // Initialize Firebase safely
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app); 
 
-export { app, auth, db };
+export { app, auth, db, storage };
